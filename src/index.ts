6ac8fc337bbaaf9bf366a023457df36173735b78
9hodev <siaskkh4884@gmail.com>
2024-08-14 14:56:16 +0700

    remove route

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import { Elysia, t } from 'elysia'
 import { swagger } from "@elysiajs/swagger";
 import userRoute from './routes/user'
-import authRoute from './routes/auth'
 
 const app = new Elysia();
 
 app.use(swagger())
 app
     .group('/api',(app)=>app.use(userRoute))
-    .group('/api',(app)=>app.use(authRoute))
     .listen(3000,()=>console.log(`server is runnnig at: http://localhost:${3000}/`))
 
 // 1 Elysia = 1 Controller (POST,GET,PUT,DELETE) CRUD
