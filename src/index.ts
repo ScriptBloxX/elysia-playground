a61854b8ef834ed34617b16ecf75ef9471ba3b8b
9hodev <siaskkh4884@gmail.com>
2024-08-15 19:38:42 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import userRoute from './routes/user'
 import { Elysia, t } from 'elysia'
 import { swagger } from "@elysiajs/swagger";
+import cors from '@elysiajs/cors';
 
 const app = new Elysia();
-// don't edit this file ma
+
 app.use(swagger())
 app
 .listen(3000,()=>console.log(`server is runnnig at: http://localhost:${3000}/`))
+    .use(cors())
+    // # ↘ Please don't edit this manually ↙ //
     .group('/api',(app)=>app.use(userRoute))
     
