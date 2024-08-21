9b6794c6801f9b6a1cac16c77514f7b7eb8dd63f
9hodev <siaskkh4884@gmail.com>
2024-08-21 16:49:54 +0700

    add middleware

---
+++ src/middleware/mw.ts
@@ -0,0 +1,3 @@
+export default async function mw() {
+    return console.log('middleware is work!'); 
+}
--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,12 +1,13 @@
 import Elysia from "elysia";
 import { Create, Delete, Read, ReadAll, Update } from "./service";
 import { Model } from "./model";
+import mw from "../../middleware/mw";
 
 export default new Elysia({prefix: '/user'})
     .use(Model)
+    .onBeforeHandle(mw) //midleware example
     .get('/',()=> ReadAll())
     .get('/:id',()=> Read())
     .post('/',({body})=> Create(body),{body: 'create',response: 'create'})
     .patch('/',()=> Update())
     .delete('/',()=> Delete())
-
