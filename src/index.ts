964bb1991e97d700987bbf70e4a5c5a27536a47c
9hodev <siaskkh4884@gmail.com>
2024-10-09 10:06:13 +0700

    add rate limitt

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import userRoute from './routes/user'
 import { Elysia, t } from 'elysia'
 import { swagger } from "@elysiajs/swagger";
 import cors from '@elysiajs/cors';
+import { rateLimit } from 'elysia-rate-limit';
 
 const app = new Elysia();
 
@@ -9,6 +10,17 @@ app.use(swagger())
 app
 .listen(3000,()=>console.log(`server is runnnig at: http://localhost:${3000}/`))
     .use(cors())
+    .use(rateLimit({
+        duration: 3000,
+        max: 1,
+        errorResponse: new Response("Rate limit reached", {
+            status: 429,
+            headers: new Headers({
+                'Content-Type': 'text/plain',
+                'Custom-Header': 'custom',
+            }),
+        }),
+    }),)
     // # ↘ Please don't edit this manually ↙ //
     .group('/api',(app)=>app.use(userRoute))
     
